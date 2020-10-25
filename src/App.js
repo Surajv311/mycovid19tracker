a6f36e167ae580964bed4eaf55d073c4d7fa0d3c
Surajv <vsuraj2405@gmail.com>
2020-10-25 23:37:59 +0530

    added dropdown using material UI - formcontrol & menuitem

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { 
+  FormControl, 
+  MenuItem,
+  Select,
+  Card,
+  CardContent, } from "@material-ui/core";
 import React from "react"; 
-import './App.css';
+import './App.css'; 
 
 function App() {
   return (
     <div className="App">
-      <h1>ureiotuoegjo</h1>
+      <h1>my_Covid19 Tracker App</h1>
+    
+    {/* FormControl tag is in MaterialUI */}
+    {/* adding BEM convention of naming */}
+    {/* adding dropdown */}
+    <FormControl className="app__dropdown">
+
+{/* adding dropdown... Select tag in MaterialUI */}
+<Select>
+{/* to give dropdowns of options we use MenuItem tag */}
+<MenuItem value = "world"> test</MenuItem>
+<MenuItem value = "world"> eqw</MenuItem>
+
+</Select>
+      
+
+
+    </FormControl>
+    
+    
     </div>
   );
 }
