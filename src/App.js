e3dd15cb824a0343d4cb43cfe6e1b0ad8e4d5e96
Surajv <vsuraj2405@gmail.com>
2020-10-26 14:03:10 +0530

    working DropDown- API data fetched

--- src/App.js
+++ src/App.js
@@ -4,15 +4,37 @@ import {
   Select,
   Card,
   CardContent, } from "@material-ui/core";
-import React , {useState} from "react"; 
+import React , {useState, useEffect} from "react"; 
 import './App.css'; 
 
 function App() {
 
 // using hooks = They let you use state and other React features without writing a class.
-const [countries, setCountries] = useState(["US", "Belgium", "Japan"]); 
+const [countries, setCountries] = useState([]); //["US", "Belgium", "Japan"]
 
-  return (
+
+// useEffect = its a hook that runs a piece of code based on the given condition 
+  
+useEffect(() => {
+  // the code inside this would run only once when the component loads and not again ... would fire off the code once...
+// async function = send a req, get it and manipulate it...
+const getCountriesData = async() => {
+  await fetch("https://disease.sh/v3/covid-19/countries") // then what we do is :
+  .then((response) => response.json()) //we take only the json part from the API data in response.json() 
+  .then((data) =>{
+const countries = data.map((country)=>({
+  name: country.country, // like United Kingdom...
+  value: country.countryInfo.iso2 // like UK...
+}));
+ 
+setCountries(countries); // we'll map through...
+  });
+};
+getCountriesData() ; // calling function 
+
+}, []);
+
+return (
     <div className="app">
         <div className="app__header"> 
         
@@ -32,12 +54,18 @@ const [countries, setCountries] = useState(["US", "Belgium", "Japan"]);
 
 {/* NOW instead of multiple menu item we must instead loop through one... */}
 
-{countries.map((country)=> (
+{/* {countries.map((country)=> (
 
   <MenuItem value = {country}> {country}</MenuItem>
 )
-)}
+)} */}
 {/* NOW Better than that we can make API call from website */}
+{countries.map((country)=> (
+
+<MenuItem value = {country.value}> {country.name}</MenuItem>
+)
+)}
+
 </Select>
       
 
