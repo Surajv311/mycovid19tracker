6ee396b035830db252e5f4e8a0747e4531165ffa
Surajv <vsuraj2405@gmail.com>
2020-10-26 11:45:09 +0530

    map function for displaying countries in dropdown

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import {
   Select,
   Card,
   CardContent, } from "@material-ui/core";
-import React from "react"; 
+import React , {useState} from "react"; 
 import './App.css'; 
 
 function App() {
+
+// using hooks = They let you use state and other React features without writing a class.
+const [countries, setCountries] = useState(["US", "Belgium", "Japan"]); 
+
   return (
     <div className="app">
         <div className="app__header"> 
         
         
-      <h1>my_Covid19 Tracker App</h1>
+      <h1>Live COVID19 Tracker App</h1>
     
     {/* FormControl tag is in MaterialUI */}
     {/* adding BEM convention of naming */}
@@ -23,9 +27,17 @@ function App() {
 {/* adding dropdown..using the variant shown... Select tag in MaterialUI */}
 <Select variant = "outlined" value = "abc">
 {/* to give dropdowns of options we use MenuItem tag */}
-<MenuItem value = "world"> test</MenuItem>
-<MenuItem value = "world"> eqw</MenuItem>
+{/* <MenuItem value = "world"> test</MenuItem>
+<MenuItem value = "world"> eqw</MenuItem> */}
 
+{/* NOW instead of multiple menu item we must instead loop through one... */}
+
+{countries.map((country)=> (
+
+  <MenuItem value = {country}> {country}</MenuItem>
+)
+)}
+{/* NOW Better than that we can make API call from website */}
 </Select>
       
 
